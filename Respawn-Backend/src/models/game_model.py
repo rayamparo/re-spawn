7529d2719b2f42df5877978695a4a5a167e8e008
Raymond Amparo <raymondamparo@Raymonds-MacBook-Pro.local>
2024-06-24 20:52:11 -0700

    changed games model

--- Respawn-Backend/src/models/game_model.py
+++ Respawn-Backend/src/models/game_model.py
@@ -1,10 +1,10 @@
 class Game:
 
-    def __int__(self, owner_id, game_id, game_release_date, game_developer, game_summary, game_status, game_list):
+    def __int__(self, owner_id, game_id, game_name, game_release_date, game_summary, game_status, game_list):
         self._owner_id = owner_id
         self._game_id = game_id
+        self._game_name = game_name
         self._game_release_date = game_release_date
-        self._game_developer = game_developer
         self._game_summary = game_summary
         self._game_status = game_status
         self._game_list = game_list
